e691787a19f5eb72c80691cddb9b90e2b061b4f7
B4Nork <now.krystia@gmail.com>
2026-08-15 18:38:22 +0200

    Added banana animations upon pressing Start

--- src/pages/Home/components/StartButton/StartButton.ts
+++ src/pages/Home/components/StartButton/StartButton.ts
@@ -6,5 +6,28 @@ export function StartButton(): HTMLElement {
     button.className = "start-button";
     button.textContent = "ZACZNIJ";
 
+    button.addEventListener("click", () => {
+        createBananas();
+    });
+
     return button;
+}
+
+function createBananas(): void {
+    for (let i = 0; i < 1200; i++) {
+        const banana = document.createElement("div");
+
+        banana.className = "falling-banana";
+        banana.textContent = "🍌";
+
+        banana.style.left = `${Math.random() * 100}%`;
+        banana.style.animationDelay = `${Math.random() * 1.5}s`;
+        banana.style.animationDuration = `${1 + Math.random() * 2}s`;
+
+        document.body.appendChild(banana);
+
+        banana.addEventListener("animationend", () => {
+            banana.remove();
+        });
+    }
 }
